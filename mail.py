7c67a1bf9433a927330835c592ef7985aa3a6140
Mikivannah <164083395+Mikivannah@users.noreply.github.com>
2024-03-22 14:23:55 +0300

    updete main,py in const

--- mail.py
+++ mail.py
@@ -3,16 +3,17 @@ import pygame
 # образуем тело цикла всей игры
 pygame.init()
 
-# организация поля игры с использованием pygame.disply константы высоты и ширины
+# организация поля игры с использованием pygame.disply /размер/имя/иконка/
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# создою названия окна программы
 pygame.display.set_caption("Игра Тир")
 
 # создаю переменую. которая подгружает иконку игры
-icon = pygame.image.load("")
+icon = pygame.image.load("img/tir.prn")
+pygame.display.set_icon(icon)
+
 
 
 running = True
