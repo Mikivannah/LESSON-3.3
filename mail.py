0d25e29db21f2f583a4732d8edb79053aeb9147f
Mikivannah <164083395+Mikivannah@users.noreply.github.com>
2024-03-22 14:00:15 +0300

    add new const

--- mail.py
+++ mail.py
@@ -3,6 +3,18 @@ import pygame
 # образуем тело цикла всей игры
 pygame.init()
 
+# организация поля игры с использованием pygame.disply константы высоты и ширины
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# создою названия окна программы
+pygame.display.set_caption("Игра Тир")
+
+# создаю переменую. которая подгружает иконку игры
+icon = pygame.image.load("")
+
+
 running = True
 
 while running:
