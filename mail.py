4c2f4b2dc14cbbd88d7bd8a8827eef879ac2b5b7
Mikivannah <164083395+Mikivannah@users.noreply.github.com>
2024-03-23 11:52:53 +0300

    add all cyclle code

--- mail.py
+++ mail.py
@@ -12,7 +12,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
 
 # создаю переменую. которая подгружает иконку игры
-icon = pygame.image.load("img/tir.prn")
+icon = pygame.image.load("img/tir.png")
 pygame.display.set_icon(icon)
 
 target_icon = pygame.image.load("img/target.png")
@@ -32,8 +32,14 @@ while running:
     screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-         running = False
+            running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_WIDTH and target_y < mouse_y < target_y + target_HEIGHT:
+                target_x = random.randint(0, SCREEN_WIDTH - target_WIDTH)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_HEIGHT)
+    screen.blit(target_icon, (target_x, target_y))
+    pygame.display.update()
+
 
 pygame.quit()
